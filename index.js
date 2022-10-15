6592252d8e145e60fd50266833cc7000dbd1deef
Wai Sze <waisze0212@yahoo.com>
2022-10-14 20:40:33 -0500

    commit tree fc58c1ba5ad6a1ccf136cb5b1714209aef806da1

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+// const num1 = 31;
+// const num2 = 2;
+function multiply(num1, num2){
+num1 = 31;
+num2 = 2;
+return num1 * num2;
+}
+
+function random() {
+    return Math.ceil(Math.random());
+}
